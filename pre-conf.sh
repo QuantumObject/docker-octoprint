1111dec85eb2adc1d92d70f6a1bc627afe95ab62
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-07-20 09:48:19 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 set -e
+
+git clone https://github.com/foosel/OctoPrint.git
+cd OctoPrint
+pip install -r requirements.txt
+python setup.py install 
+
+
+usermod -a -G tty octoprint
+usermod -a -G dialout octoprint
