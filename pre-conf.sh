1d8bfcc927e9377b996bb32d08805ad8bd6ba56f
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2018-09-07 02:12:55 -0400

    adding CuraEngine
    adding CuraEngine

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
+# installing OctoPrint
 git clone https://github.com/foosel/OctoPrint.git
 cd OctoPrint
 pip install -r requirements.txt
 python setup.py install 
 
+# installing CuraEngine
+cd opt/
+git clone -b legacy https://github.com/Ultimaker/CuraEngine.git
+cd CuraEngine/
+make
